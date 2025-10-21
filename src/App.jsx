6d971cee4c26fc0f662ff738f0b4290b3687d4c1
leticiaArauj0 <leticia.araujo@gmail.com>
2025-10-21 19:59:17 -0400

    feat: add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 
 export function App() {
   return (
     <Routes>
       <Route path='/' element={<Register/>}></Route>
+      <Route path='login' element={<Login/>}></Route>
     </Routes>
   )
 }
